34fca413bbbc5fe707c86df044354293a58f12c2
Andrii Moisol <andrii.moisol@gmail.com>
2020-04-29 17:42:36 +0300

    Add image class and read size, width & height

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import struct
+
+
 class Pixel(object):
     def __init__(self, r, g, b):
         self.r = r
@@ -8,7 +11,20 @@ class Pixel(object):
         return f"red: {self.r}, green: {self.g}, blue: {self.b}"
 
 
-pixel = Pixel(100, 200, 40)
-assert str(pixel) == "red: 100, green: 200, blue: 40"
+class Image(object):
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.pixels = []
 
+    def _read(self, file_name):
+        f = open(file_name, 'rb')
+        f.seek(2)
+        file_size = struct.unpack('<L', f.read(4))[0]
+        f.seek(18)
+        self.width, self.height = struct.unpack('<LL', f.read(8))
+        f.close()
 
+image = Image()
+image._read('bmp.bmp')
+print(image.width, image.height)
